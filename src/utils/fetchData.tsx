441fa7b442029ec2a30ec6df05e3a4ced68cb226
liuhaoyu1994 <liuhaoyuca@gmail.com>
2024-01-31 18:28:35 -0800

    fix: remove test console log

--- src/utils/fetchData.tsx
+++ src/utils/fetchData.tsx
@@ -1,7 +1,7 @@
 const backendUrl = "http://localhost:3000"
 
 export async function getAllCourts() {
-    console.log("trigger")
+    //console.log("trigger")
     const res = await fetch(`${backendUrl}/courts/allcourts`)
     const data = await res?.json()
     return data;
